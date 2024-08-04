e370bc7f0dc6a4d938fecca803fac83f7012a594
Ana Garcia <anagarcia3174@gmail.com>
2024-08-03 21:18:34 -0500

    Added client and server urls

--- client/src/services/axios.js
+++ client/src/services/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://movie-app-server-miyf2ixt5-anas-projects-93313cc1.vercel.app/",
+  baseURL: "https://movie-app-server-wine.vercel.app",
 });
 
 export default instance;

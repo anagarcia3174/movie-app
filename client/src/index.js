501ad12ca9fecb5e4730ef18e1ba02dd206cf2a1
Ana Garcia <anagarcia3174@gmail.com>
2025-01-02 21:41:07 -0600

    Trying vercel analytics

--- client/src/index.js
+++ client/src/index.js
@@ -6,11 +6,17 @@ import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { store } from "./redux/store";
 import { Provider } from "react-redux";
+import { SpeedInsights } from "@vercel/speed-insights/react"
+import { Analytics } from "@vercel/analytics/react"
+
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
     <App />
+    <Analytics/>
+    <SpeedInsights/>
   </Provider>
 );
 

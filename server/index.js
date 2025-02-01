e447d41c4b050bb3d041f0d22e13ca14bf0a5bdc
Ana Garcia <anagarcia3174@gmail.com>
2025-02-01 12:00:40 -0600

    Changed env variable name

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ admin.initializeApp({
 });
 
 app.use(cors({
-  origin: [process.env.VERCEL_CLIENT_URL, process.env.EXTENSION_URL, process.env.VERCEL_URL],
+  origin: [process.env.VERCEL_CLIENT_URL, process.env.EXTENSION_URL, process.env.CUSTOM_CLIENT_URL],
   methods: ['GET', 'POST', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true

df4f4807bbf1c9db06e2bd14b9f66d293d476a72
Ana Garcia <anagarcia3174@gmail.com>
2025-02-01 10:42:40 -0600

    Added new url to cors options

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ admin.initializeApp({
 });
 
 app.use(cors({
-  origin: [process.env.VERCEL_CLIENT_URL, process.env.EXTENSION_URL],
+  origin: [process.env.VERCEL_CLIENT_URL, process.env.EXTENSION_URL, process.env.VERCEL_URL],
   methods: ['GET', 'POST', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
